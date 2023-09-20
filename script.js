92a0421bdea06e3e743b690e419a9003b360c349
MadziaSzmatloch <magdaszmatloch@gmail.com>
2023-09-20 16:02:03 +0200

    saving current url added

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
+const tabBtn = document.getElementById("tab-btn")
 const deleteBtn = document.getElementById("delete-btn")
 const ulEl = document.getElementById("ul-el")
 
@@ -17,6 +18,14 @@ inputBtn.addEventListener("click", function() {
 
     localStorage.setItem("myLeads", JSON.stringify(myLeads))
     render(myLeads)
+})
+
+tabBtn.addEventListener("click", function(){    
+    chrome.tabs.query({active: true, lastFocusedWindow: true}, function(tabs){
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+        render(myLeads)
+    })
 })
 
 deleteBtn.addEventListener("dblclick", function() {
