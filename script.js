e70a1d7e774606d8c42444ea279d57436c1fd94d
MadziaSzmatloch <magdaszmatloch@gmail.com>
2023-09-20 12:25:12 +0200

    saving memory in localStorage

--- script.js
+++ script.js
@@ -1,21 +1,37 @@
 let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
+const deleteBtn = document.getElementById("delete-btn")
 const ulEl = document.getElementById("ul-el")
+
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+
+if(leadsFromLocalStorage){
+    myLeads = leadsFromLocalStorage
+    render(myLeads)
+}
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
     inputEl.value = ""
-    renderLeads()
+
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    render(myLeads)
+})
+
+deleteBtn.addEventListener("dblclick", function() {
+    localStorage.clear()
+    myLeads = []
+    render(myLeads)
 })
 
 
-function renderLeads() {
+function render(leads) {
     let listItems = ""
-    for (let i = 0; i < myLeads.length; i++){
+    for (let i = 0; i < leads.length; i++){
         listItems += `
             <li>
-                <a href=" ${myLeads[i]} " target="_blank">  ${myLeads[i]}  </a>
+                <a href=" ${leads[i]} " target="_blank">  ${leads[i]}  </a>
             </li>` 
     }
     ulEl.innerHTML = listItems
